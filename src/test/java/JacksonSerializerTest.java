0ebbaa28bbb0af9d2c74cc77eedfcc6585cf277a
Michael Kunze <mkunze@inoxio.de>
2018-09-24 12:11:57 +0200

    Add more tests to show different possibilities

--- src/test/java/JacksonSerializerTest.java
+++ src/test/java/JacksonSerializerTest.java
@@ -1,29 +1,75 @@
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonSerializerTest {
 
-  @Test
-  public void shouldTransformJson() throws IOException {
+    @Test
+    public void shouldTransformSimpleMetric() throws IOException {
 
-    // given
-    final ObjectMapper mapper = new ObjectMapper();
-    final String json = FileUtils.readFileToString(
-            new File(getClass().getClassLoader().getResource("dashboard.json").getFile()), "UTF-8");
+        // given
+        final ObjectMapper mapper = new ObjectMapper();
+        //language=JSON
+        final String json = "{\"metrics\":[[\"StringA\",\"StringB\"]]}";
 
-    // when
-    final PropertiesDTO properties = mapper.readValue(json, PropertiesDTO.class);
-    final String result = mapper.writeValueAsString(properties);
+        // when
+        final PropertyDTO property = mapper.readValue(json, PropertyDTO.class);
+        final String serializedJson = mapper.writeValueAsString(property);
 
-    // then
-    assertThat(result, is(json));
-  }
+        // then
+        assertThat(serializedJson, is(json));
+    }
+
+    @Test
+    public void shouldTransformMetricWithRenderingProperty() throws IOException {
+
+        // given
+        final ObjectMapper mapper = new ObjectMapper();
+        //language=JSON
+        final String json = "{\"metrics\":[[\"StringA\",\"StringB\",{\"label\":\"some label\"}]]}";
+
+        // when
+        final PropertyDTO property = mapper.readValue(json, PropertyDTO.class);
+        final String serializedJson = mapper.writeValueAsString(property);
+
+        // then
+        assertThat(serializedJson, is(json));
+    }
+
+    @Test
+    public void shouldTransformMultipleMetrics() throws IOException {
+
+        // given
+        final ObjectMapper mapper = new ObjectMapper();
+        //language=JSON
+        final String json = "{\"metrics\":[[\"StringA\",\"StringB\"],[\"StringC\",\"StringD\",\"StringE\",\"StringF\"]]}";
+
+        // when
+        final PropertyDTO property = mapper.readValue(json, PropertyDTO.class);
+        final String serializedJson = mapper.writeValueAsString(property);
+
+        // then
+        assertThat(serializedJson, is(json));
+    }
+
+    @Test
+    public void shouldTransformMultipleMetricsWithRenderingProperty() throws IOException {
+
+        // given
+        final ObjectMapper mapper = new ObjectMapper();
+        //language=JSON
+        final String json = "{\"metrics\":[[\"StringA\",\"StringB\",{\"label\":\"some label\"}],[\"StringC\",\"StringD\",\"StringE\",\"StringF\",{\"color\":\"some color\",\"label\":\"some other label\"}]]}";
+
+        // when
+        final PropertyDTO property = mapper.readValue(json, PropertyDTO.class);
+        final String serializedJson = mapper.writeValueAsString(property);
+
+        // then
+        assertThat(serializedJson, is(json));
+    }
 }
